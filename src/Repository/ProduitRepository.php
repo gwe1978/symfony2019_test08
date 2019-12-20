b19fca4c29403b2c71f83ca4cd85091f85d20f26
gwe <guilhem.wetteren@condorcet.be>
2019-12-20 16:41:45 +0100

    Ajout de commentaires dans les classes "repository"

--- src/Repository/ProduitRepository.php
+++ src/Repository/ProduitRepository.php
@@ -14,6 +14,7 @@ use Doctrine\Common\Persistence\ManagerRegistry;
  */
 class ProduitRepository extends ServiceEntityRepository
 {
+    //////////////////////////////////////
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Produit::class);
